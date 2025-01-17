35dfd06d8168ec7f25025d03d7119bb303288453
pos1234 <paulosegebeyehu5@gmail.com>
2025-01-17 11:44:26 +0300

    Updated package.json to generate prisma client

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { PrismaClient } from "@prisma/client/edge"
+import { PrismaClient } from '@prisma/client';
+
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor(config:ConfigService) {
